b83e25ef174c31dcb2755ff30db220e32ecc6a39
Emanuel Bitkov <emanuel.bitkov@regenta-verlag.de>
2022-05-13 13:43:36 +0200

    plugin logic

---
+++ src/AppLockComposerPlugin.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace ebitkov\AppLockComposerPlugin;
+
+use Composer\Composer;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+
+class AppLockComposerPlugin implements PluginInterface
+{
+    private IOInterface $io;
+    private Composer $composer;
+
+    private ?string $rootDir;
+
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            'pre-install-cmd' => 'lockApp',
+            'pre-update-cmd' => 'lockApp',
+            'post-install-cmd' => 'unlockApp',
+            'post-update-cmd' => 'unlockApp'
+        ];
+    }
+
+
+    public function lockApp()
+    {
+        file_put_contents($this->rootDir . '/.app_lock', 'APP_LOCKED=true');
+        $this->io->info('app locked for update');
+    }
+
+    public function unlockApp()
+    {
+        file_put_contents($this->rootDir . '/.app_lock', 'APP_LOCKED=false');
+        $this->io->info('app unlocked');
+    }
+
+
+    public function activate(Composer $composer, IOInterface $io)
+    {
+        $this->io = $io;
+        $this->composer = $composer;
+
+        $this->rootDir = $composer->getPackage()->getTargetDir();
+    }
+
+    public function deactivate(Composer $composer, IOInterface $io)
+    {
+    }
+
+    public function uninstall(Composer $composer, IOInterface $io)
+    {
+    }
+}
